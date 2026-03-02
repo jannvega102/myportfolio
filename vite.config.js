258a9e96608fd00020e3e97f618c3ed32315ab84
Jann Niño Armar Trufil Vega <141836123+jannvega102@users.noreply.github.com>
2026-03-02 22:20:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // This forces the Request URL to become https://jannvega102.github.io/-/src/main.js
-  base: '/-/', 
+  base: './', 
   build: {
     outDir: 'dist',
   }
